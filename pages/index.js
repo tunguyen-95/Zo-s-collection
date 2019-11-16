624640f78de7250fc95a039835b831cf69e7134a
tunguyen-95 <thunguyen.tt@outlook.com>
2019-11-16 20:09:42 +0100

    check which kind of request made

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+
 function Home() {
+  React.useEffect(() => {
+    getProduct();
+  }, []);
+  function getProduct() {
+    const url = 'http://localhost:3000/api/products';
+    axios.get(url);
+  }
+
   return <>home</>;
 }
 
