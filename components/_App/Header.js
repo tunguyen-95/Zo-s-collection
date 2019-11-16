252e9f51a932b02182a01a50771480fae7df3c2e
tunguyen-95 <thunguyen.tt@outlook.com>
2019-11-16 19:23:06 +0100

    adjust the display of header items

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,6 +1,8 @@
 import { Menu, Container, Image, Icon } from 'semantic-ui-react';
 import Link from 'next/link';
 function Header() {
+  const user = false;
+
   return (
     <Menu fluid id="menu" inverted>
       <Container text>
@@ -20,34 +22,43 @@ function Header() {
             Cart
           </Menu.Item>
         </Link>
-        <Link href="/create">
-          <Menu.Item header>
-            <Icon name="add square" size="" />
-            Create
-          </Menu.Item>
-        </Link>
-        <Link href="/account">
-          <Menu.Item header>
-            <Icon name="user " size="large" />
-            Account
-          </Menu.Item>
-        </Link>
-        <Menu.Item header>
-          <Icon name="sign out " size="large" />
-          Logout
-        </Menu.Item>
-        <Link href="/login">
-          <Menu.Item header>
-            <Icon name="sign in " size="large" />
-            Login
-          </Menu.Item>
-        </Link>
-        <Link href="/signup">
-          <Menu.Item header>
-            <Icon name="signup " size="large" />
-            Signup
-          </Menu.Item>
-        </Link>
+        {user && (
+          <Link href="/create">
+            <Menu.Item header>
+              <Icon name="add square" size="" />
+              Create
+            </Menu.Item>
+          </Link>
+        )}
+        {user ? (
+          <>
+            <Link href="/account">
+              <Menu.Item header>
+                <Icon name="user " size="large" />
+                Account
+              </Menu.Item>
+            </Link>
+            <Menu.Item header>
+              <Icon name="sign out " size="large" />
+              Logout
+            </Menu.Item>
+          </>
+        ) : (
+          <>
+            <Link href="/login">
+              <Menu.Item header>
+                <Icon name="sign in " size="large" />
+                Login
+              </Menu.Item>
+            </Link>
+            <Link href="/signup">
+              <Menu.Item header>
+                <Icon name="signup " size="large" />
+                Signup
+              </Menu.Item>
+            </Link>
+          </>
+        )}
       </Container>
     </Menu>
   );
