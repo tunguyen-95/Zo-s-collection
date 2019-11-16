134c778ce35ec728a20494ce066d375a4ebe619c
tunguyen-95 <thunguyen.tt@outlook.com>
2019-11-16 20:45:23 +0100

    the data fetched from the server

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import axios from 'axios';
 
-function Home(props) {
-  console.log(props);
-  React.useEffect(() => {
-    getProduct();
-  }, []);
-  async function getProduct() {
-    const url = 'http://localhost:3000/api/products';
-    const response = await axios.get(url);
-    console.log(response.data);
-  }
+function Home(products) {
+  console.log(products);
+  // React.useEffect(() => {
+  //   getProduct();
+  // }, []);
+  // async function getProduct() {
+  //   const url = 'http://localhost:3000/api/products';
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  // }
 
   return <>home</>;
 }
 
-Home.getInitialProps = () => {
+Home.getInitialProps = async () => {
   //fetch data on server
   //return response data as an object
   // this object will be merged
-  return { hello: 'world ' };
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  return { products: response.data };
 };
 export default Home;
